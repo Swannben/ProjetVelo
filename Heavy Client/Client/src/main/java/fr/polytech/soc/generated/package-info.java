9cd1c2adaed71b6cc5160b8014d27d0a3568e9f3
SkanderMeziou <skander.meziou@etu.unice.fr>
2023-12-05 17:23:03 +0100

    mvp

--- Heavy Client/Client/src/main/java/fr/polytech/soc/generated/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://schemas.datacontract.org/2004/07/ProjetVelo", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package fr.polytech.soc.generated;
